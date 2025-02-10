61e0addbaada99f6cc29bd4a8d2813c1b227e3c0
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2025-02-10 09:17:49 +0100

    feat: Add update product feature and image upload to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 get '/products' do
-  @products = Product.all
+  @products = Product.where(active: 1)
 
   erb :"products/index"
 end
@@ -13,27 +13,78 @@ get '/products/new' do
 end
 
 post '/products' do
-  if store_product(params[:name], params[:product_type], params[:kcal], params[:fat], params[:proteins],
-                   params[:carbohydrates])
+  if store_product(params)
     redirect '/products'
   else
     redirect '/products/new'
   end
 end
 
+get '/products/:id/edit' do |id|
+  @product = Product.find_by(id: id)
+  @product_types = ProductType.all
+
+  erb :"products/edit"
+end
+
+patch '/products/:id' do |id|
+  if update_product(id, params)
+    redirect '/products'
+  else
+    redirect "/products/#{id}/edit"
+  end
+end
+
 delete '/products/:id' do |id|
-  redirect '/products' if Product.destroy_by(id: id)
+  redirect '/products' if Product.find_by(id: id).update(active: 0)
 end
 
 #######################################
 
-def store_product(name, product_type, kcal, fat, proteins, carbohydrates)
+def store_product(params)
+  image_path = store_image(params[:image])
+
   true if Product.create(
-    name: name,
-    product_type_id: product_type.to_i,
-    kcal: kcal.to_i,
-    fat: fat.to_i,
-    proteins: proteins.to_i,
-    carbohydrates: carbohydrates.to_i
+    name: params[:name],
+    product_type_id: params[:product_type].to_i,
+    kcal: params[:kcal].to_i,
+    fat: params[:fat].to_i,
+    proteins: params[:proteins].to_i,
+    carbohydrates: params[:carbohydrates].to_i,
+    image: image_path
   )
+end
+
+def update_product(id, params)
+  product = Product.find_by(id: id)
+  image_path = store_image(params[:image])
+
+  puts image_path = product.image if image_path.nil?
+
+  true if product.update(
+    name: params[:name],
+    product_type_id: params[:product_type].to_i,
+    kcal: params[:kcal].to_i,
+    fat: params[:fat].to_i,
+    proteins: params[:proteins].to_i,
+    carbohydrates: params[:carbohydrates].to_i,
+    image: image_path
+  )
+end
+
+def store_image(image)
+  puts image
+  return nil if image.nil? || !image.is_a?(Sinatra::IndifferentHash) || !image[:type].start_with?('image/')
+
+  FileUtils.mkdir_p('public/images') unless Dir.exist?('public/images')
+
+  unless image.nil?
+    image_type = image[:type].delete_prefix('image/')
+    image_name = "product_#{Dir.entries('public/images').size}.#{image_type}"
+    tempfile = image[:tempfile]
+    final_path = "public/images/#{image_name}"
+    FileUtils.cp(tempfile.path, final_path)
+  end
+
+  final_path
 end
